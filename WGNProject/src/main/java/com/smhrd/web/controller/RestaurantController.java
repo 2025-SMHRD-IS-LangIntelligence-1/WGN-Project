15f251c1e0f541cd104a6af03f343f6d6cb7219b
donggyun <ldgsmart@gmail.com>
2025-08-05 08:50:46 +0900

    쉴래요

--- WGNProject/src/main/java/com/smhrd/web/controller/RestaurantController.java
+++ WGNProject/src/main/java/com/smhrd/web/controller/RestaurantController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/restaurant")
 @Controller
 public class RestaurantController {
 
-    @GetMapping("/restaurant")
+    @GetMapping()
     public String restaurantPage() {
     	
         return "restaurant/restaurant";
