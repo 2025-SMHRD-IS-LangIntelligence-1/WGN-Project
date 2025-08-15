292ac7e545180a1604be71b74b851220da7ae40b
hyerim <hyerim8483@gmail.com>
2025-08-15 10:37:50 +0900

    사용자 로그 기록 관련 버그 수정

--- WGNProject/src/main/java/com/smhrd/web/controller/SearchController.java
+++ WGNProject/src/main/java/com/smhrd/web/controller/SearchController.java
@@ -120,11 +120,14 @@ public class SearchController {
 	    }
 	    
 	    t_member member = (t_member) session.getAttribute("member");
-	    String mb_id = member.getMb_id();
 	    
-	    // 검색 로그 저장
-	    if (mb_id != null) {
-	    	memberService.saveLog(mb_id, resIdxList.get(0), "검색");
+	    if (member != null) {
+	    	String mb_id = member.getMb_id();
+		    
+		    // 검색 로그 저장
+		    if (mb_id != null) {
+		    	memberService.saveLog(mb_id, resIdxList.get(0), "검색");
+		    }
 	    }
 	    
 	    return resList;
